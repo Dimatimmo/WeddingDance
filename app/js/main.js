c270a4034a23626b83c69178a5a1e90a3487bef8
Dimatimmo <dimatimmokh@gmail.com>
2019-11-19 19:54:36 +0200

    slick redesign

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+jQuery(function() {
+  initToggleClass();
+  initSlick();
+});//end ready
+
+function initToggleClass() {
+	$('.menu-trigger').on('click', function(e) {
+		e.preventDefault();
+		$('html').toggleClass('menu-active');
+	});//end slide toggle
+}
+
+function initSlick(){
+  $('.cards__holder').slick({
+    mobileFirst: true,
+    infinite: false,
+    dots: true,
+    arrows: false,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: 'unslick'
+      }
+    ]
+  });
+}
